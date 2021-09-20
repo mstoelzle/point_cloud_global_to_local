f4984b9203a05c2c4d26dd99606154f76ba489f1
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-20 23:34:25 +0200

    Implement loading of ply file

--- src/point_cloud_global_to_local_node.cpp
+++ src/point_cloud_global_to_local_node.cpp
@@ -8,6 +8,10 @@
 
 #include <ros/ros.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+
 #include <grid_map_core/GridMap.hpp>
 #include <grid_map_ros/GridMapRosConverter.hpp>
 
@@ -21,10 +25,26 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh("~");
   gm::setVerbosityLevelToDebugIfFlagSet(nh);
 
-  ros::Publisher gridMapPub;
-  gridMapPub = nh.advertise<grid_map_msgs::GridMap>("grid_map_from_raw_pointcloud", 1, true);
+  ros::Publisher localPointCloudPub;
+  std::string local_point_cloud_topic;
+  nh.param<std::string>("local_point_cloud_topic", local_point_cloud_topic, "local_point_cloud");
+  localPointCloudPub = nh.advertise<sensor_msgs::PointCloud2>(local_point_cloud_topic, 1, true);
 
-  grid_map::GridMapPclLoader gridMapPclLoader;
+  ros::Subscriber robotPoseSub;
+  std::string robot_pose_topic;
+  nh.param<std::string>("robot_pose_topic", robot_pose_topic, "/slam/pose");
+  // robotPoseSub = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>(robot_pose_topic, 1, true);
+
+  std::string point_cloud_path;
+  nh.param<std::string>("point_cloud_path", point_cloud_path, "/mnt/c/Users/mstolzle/Documents/ethz/MT/anymal-datasets/hoenggerberg/leica-maps/ETH_HPH_binary.ply");
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPLYFile<pcl::PointXYZ> (point_cloud_path, *cloud) == -1)
+  {
+    PCL_ERROR ("Couldn't read pcd file\n");
+    return EXIT_FAILURE;
+  }
+
+  /* grid_map::GridMapPclLoader gridMapPclLoader;
   const std::string pathToCloud = gm::getPcdFilePath(nh);
   gridMapPclLoader.loadParameters(gm::getParameterPath());
   gridMapPclLoader.loadCloudFromPcdFile(pathToCloud);
@@ -34,13 +54,11 @@ int main(int argc, char** argv) {
   grid_map::GridMap gridMap = gridMapPclLoader.getGridMap();
   gridMap.setFrameId(gm::getMapFrame(nh));
 
-  gm::saveGridMap(gridMap, nh, gm::getMapRosbagTopic(nh));
-
-  // publish grid map
+  // publish point cloud
 
   grid_map_msgs::GridMap msg;
   grid_map::GridMapRosConverter::toMessage(gridMap, msg);
-  gridMapPub.publish(msg);
+  gridMapPub.publish(msg); */
 
   // run
   ros::spin();
